c8448fe5e008d466d6837fcd7c3b23ba3d69732e
FASEE KHAN <su92-bscsm-f23-275@gmail.com>
2026-01-05 19:00:01 +0500

    again5

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Nav from '../utility/Nav'
-import Fotter from '../utility/fotter'
+import Fotter from '../utility/Fott'
 import { Outlet } from 'react-router-dom'
 
 
